faeea253d7ec902c3915a49f06edd74c20b73235
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2021-09-08 15:04:56 +0300

    Update LRU_cache.py

--- LRU_cache.py
+++ LRU_cache.py
@@ -1,4 +1,3 @@
-from collections import deque
 from inspect import signature, getcallargs
 import time
 import redis
@@ -14,9 +13,7 @@ r = redis.Redis(
 def LRU_cache(max_len):
     def wrapper(func_to_cache):
         def get_args(*args, **kwargs):
-            print("Was it cached?")
             param_dict = {}
-            args_dict = getcallargs(func_to_cache, *args, *kwargs)
             sig = signature(func_to_cache)
             bnd = sig.bind(*args, **kwargs)
             for param in sig.parameters.keys():
@@ -24,13 +21,10 @@ def LRU_cache(max_len):
                    param_dict[param] = bnd.arguments[param]
                else:
                    param_dict[param] = sig.parameters[param].default
-            print(param_dict)       
             lru_cache ='cache'
             hash_=str(param_dict).encode()
             members = r.lrange(lru_cache,0,-1)
-            print(members)
             if hash_ not in members:
-                print("No")
                 res = func_to_cache(*args, **kwargs)
                 if len(members)<max_len:
                    r.lpush(lru_cache, hash_)
@@ -41,25 +35,9 @@ def LRU_cache(max_len):
                 r.set(hash_, pickle.dumps(res))
                 return res
             else:
-                print("Yes")
                 if len(members)<max_len:
                     r.lrem(lru_cache,0, hash_)
                     r.lpush(lru_cache, hash_)
                 return pickle.loads(r.get(hash_))
         return get_args
     return wrapper
-
-'''@LRU_cache (max_len =5)
-def cap_str(strings, descending = True, mult = 1, capital = True):
-    time.sleep(2)
-    res = []    
-    for s in strings:
-        res.append(s.upper()*mult)
-    return sorted(res, reverse = descending)     
-
-if __name__='__mailn__':
-    str_list = ['a','b','a','c','b','d','e','g']
-    r.flushall()    
-    for st in str_list:
-        print(cap_str(st, False))
-'''    
